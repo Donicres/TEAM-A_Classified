61941fd0d53b8de47b41c7a7fdba07811e5f5b00
donadoni <tangidonires@gmail.com>
2023-10-08 20:53:03 +0200

    db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
-// const express = require("express")
-// const mysql = require("mysql2")
+const express = require("express")
+const mysql = require("mysql2")
 
 
-// const db = mysql.createPool({
-//     host: "127.0.0.1",
-//     port: 3306,
-//     database: "mcqquestions_n_answers",
-//     user: "root",
-//     password: "Michealtutu123"
-//   }).promise()
+const db = mysql.createPool({
+    host: "127.0.0.1",
+    port: 3306,
+    database: "mcqquestions_n_answers",
+    user: "root",
+    password: "Michealtutu123"
+  }).promise()
   
 
 //   db.connect((err) =>{
@@ -20,25 +20,26 @@
 // });
 
 
-//    module.exports = db
+   module.exports = db
 
 
 
-const mysql = require('mysql2')
-const db = mysql.createConnection({
-    host: 'localhost',
-    user : 'root',
-    password : 'General123@',
-    database : 'TestWork'
-})
 
-db.connect((err) =>{
-    if(err){
-        console.log('Database Not Connected')
-    }else{
-        console.log('Database Connected')
-    }
-});
+// const mysql = require('mysql2')
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user : 'root',
+//     password : 'General123@',
+//     database : 'TestWork'
+// })
+
+// db.connect((err) =>{
+//     if(err){
+//         console.log('Database Not Connected')
+//     }else{
+//         console.log('Database Connected')
+//     }
+// });
 
 
-module.exports = db 
+// module.exports = db 
