3f094515bc2d9a8650a2774600df2952c711721d
denistasingole <denistasingole@gmail.com>
2023-10-11 22:22:16 +0100

    denis

--- src/routes/category.js
+++ src/routes/category.js
@@ -4,9 +4,24 @@ const db = require("../config/db")
 
 
 
-router.post('/category/create ', (req, res) => {
+
+router.post('/category/insert', async(req, res) => {
+  
+    try{
+    const {title,created_on} = req.body;
+    if(!title && !created_on){
+      res.send('Please Enter Values')
+    }
+    const titlevalue = [title];
+    const myquery = "INSERT INTO Category (title,created_on) VALUES(?,now())";
+    const result = await queryPromise(myquery,titlevalue);
+    res.send('Successfully inserted')
+    res.json({id: result.insertId,title,created_on}) // to display last add...
     
-});
+    }catch(err){
+      console.log(err)
+    }
+  });//Denis
 
 router.get('/category/:categoryID ', (req, res) => {
   
